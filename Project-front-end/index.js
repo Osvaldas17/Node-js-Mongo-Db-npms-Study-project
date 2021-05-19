786ac1c80dbc7b4541d7ce255fed3a3c12e4fa9c
Osvaldas17 <osvaldasmazeika@gmail.com>
2021-05-19 23:19:46 +0300

    initial front page

--- Project-front-end/index.js
+++ Project-front-end/index.js
@@ -1,6 +1,8 @@
 const regWin = document.querySelector('.main-container')
+const signInGreen = document.querySelector('#sign-in-green')
+const signUpGreen = document.querySelector('#sign-up-green')
 
-// regiter
+// register
 document.querySelector('#getStarted').addEventListener('click', () => {
     document.querySelector('.register-window').classList.toggle('active')
     document.querySelector('.white-back').classList.toggle('active3')
@@ -11,15 +13,23 @@ document.querySelector('#exitBtn').addEventListener('click', () => {
     document.querySelector('.white-back').classList.toggle('active3')
 })
 
+signUpGreen.addEventListener('click',() => {
+    document.querySelector('.login-window').classList.toggle('active2')
+    document.querySelector('.register-window').classList.toggle('active')
+})
+
 // login
 document.querySelector('#logIn').addEventListener('click', (e) => {
     document.querySelector('.login-window').classList.toggle('active2')
     document.querySelector('.white-back').classList.toggle('active3')
-    
-
 })
 
 document.querySelector('#exitBtn-login').addEventListener('click', () => {
     document.querySelector('.login-window').classList.toggle('active2')
     document.querySelector('.white-back').classList.toggle('active3')
+})
+
+signInGreen.addEventListener('click',() => {
+    document.querySelector('.login-window').classList.toggle('active2')
+    document.querySelector('.register-window').classList.toggle('active')
 })
