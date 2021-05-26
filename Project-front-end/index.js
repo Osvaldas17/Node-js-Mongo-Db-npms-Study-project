4b77809c437b12df438853b4b1ebd0f66d2da983
Osvaldas17 <osvaldasmazeika@gmail.com>
2021-05-26 20:35:21 +0300

    Revert "vilmanto merge"
    This reverts commit f5b9b06c702c4978b16325dfd086e963f895749d, reversing
    changes made to fd497268596dafbaf9636ef4c34ef6c55f0d1b56.

--- Project-front-end/index.js
+++ Project-front-end/index.js
@@ -9,12 +9,6 @@ window.addEventListener('DOMContentLoaded', () => {
     token = localStorage.getItem('projectauth')
 
     if (token) return window.location.href = 'loggedInMainPage.html'
-<<<<<<< HEAD
-=======
-
-    getTrendingArticlesMain()
-    getAllArticlesMain()
->>>>>>> Vilmanto
 })
 
 // register
@@ -124,92 +118,4 @@ document.getElementById('sign-up-btn').addEventListener('click', async (event) =
 
     document.querySelector('.login-window').classList.toggle('active2')
     document.querySelector('.register-window').classList.toggle('active')
-<<<<<<< HEAD
 })
-=======
-})
-
-// Render articles in main page
-
-const showSixTrendingArticlesInMain = (articles) => {
-    for (let article of articles) {
-        let sixTrendingArticles = `<div class="trend-con">
-                    <div class="trend-number">
-                        <span>${articles.indexOf(article) + 1}</span>
-                    </div>
-                    <div class="trend-content">
-                        <div class="d-flex align-center">
-                            <img class="article-user-image" src="${article.userId && article.userId.profileImage ? article.userId.profileImage : "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_640.png"}" alt="">
-                            <span class="m-l-8">${article.userId ? article.userId.userName : 'anonymous'}</span>
-                        </div>
-                        <div>
-                            <h3>${article.title}</h3>
-                            <span class="time-trend">${article.createdAt.slice(2,10)} · ${calcReadTimeInMain(article.content.length)}</span>
-                        </div>
-                    </div>
-                </div>`
-        let articleContainer = document.getElementById('top-trendsMain')
-        articleContainer.innerHTML += sixTrendingArticles
-    }
-}
-
-
-function renderLimitedContentInMain(x) {
-    x = x.split(' ').slice(0, 15)
-    x = x.join(' ')
-    return x + '...'
-}
-
-function calcReadTimeInMain(x) {
-    x = x / 300
-    let y = Math.ceil(x)
-    return y + ' min read'
-}
-
-const getTrendingArticlesMain = async () => {
-    let response = await fetch(`${url}/getTrendingArticles`, {
-        method: 'GET'
-    })
-
-    let articles = await response.json()
-    console.log('6 trending articles',articles)
-    showSixTrendingArticlesInMain(articles)
-}
-
-const getAllArticlesMain = async () => {
-    let response = await fetch(`${url}/article`, {
-        method: 'GET'
-    })
-
-    let articles = await response.json()
-    showAllArticlesMain(articles)
-}
-
-const showAllArticlesMain = (articles) => {
-    for (let article of articles) {
-        let allArticles = `<div class="article-container">
-                    <div class="article-text">
-                        <div class="d-flex align-center">
-                            <img class="article-user-image" src="${article.userId && article.userId.profileImage ? article.userId.profileImage : "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_640.png"}" alt="">
-                            <span class="m-l-8">${article.userId ? article.userId.userName : 'anonymous'}</span>
-                        </div>
-                        <h3 id="artiTittle">${article.title}</h3>
-                        <p>${renderLimitedContentInMain(article.content)}</p>
-                        <span class="time-trend">${article.createdAt.slice(2,10)} · ${calcReadTimeInMain(article.content.length)}</span>
-                    </div>
-                    <div class="article-image">
-                        <img src="${article.mainArticleImage ? article.mainArticleImage : "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png"}" alt="">
-                    </div>
-                </div>`
-        let articleContainer = document.getElementById('all-article-zone')
-        articleContainer.innerHTML += allArticles
-
-        document.querySelector('#artiTittle').addEventListener('click', () => {
-            document.querySelector('.register-window').classList.toggle('active')
-            document.querySelector('.white-back').classList.toggle('active3')
-        })
-    }
-}
-
-
->>>>>>> Vilmanto
